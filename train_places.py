b7b6f0c20dd5d6a5677435cde923128cb74e0ddd
makiit <mohammadali.khan13@gmail.com>
2022-04-08 02:35:23 -0700

    DSDS

--- train_places.py
+++ train_places.py
@@ -63,7 +63,7 @@ best_prec1 = 0
 def main():
     global args, best_prec1
     args = parser.parse_args()
-    print args
+    # print args
     # create model
     print("=> creating model '{}'".format(args.arch))
     if args.arch.lower().startswith('wideresnet'):
