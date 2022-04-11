be88c8964f2b7c91920089cd1ab93be37a26fe69
makiit <mohammadali.khan13@gmail.com>
2022-04-11 12:04:55 -0700

    DSD

--- launch-job.sh
+++ launch-job.sh
@@ -24,4 +24,4 @@ nvcc -V
 
 
 # run your code
-python train_places.py --data /expanse/lustre/projects/ddp390/makhan1/places365_standard 
+python train_places2.py -trainpath /expanse/lustre/projects/ddp390/makhan1/places365_small/train -testpath /expanse/lustre/projects/ddp390/makhan1/places365_standard/val -w 4 -b 256
